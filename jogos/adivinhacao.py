1e93cb84dc88be10c1128f1582c202f6e1ab7086
Luís Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2022-01-17 23:06:43 -0300

    Aula 3 do primeiro curso de Python 3 da Aula.

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,9 +8,16 @@ chute = int(input("Digite o seu número: "))
 
 print("Você digitou ", chute)
 
-if numero_secreto == chute:
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if acertou:
     print("Você Acertou!!")
-else:
-    print("Você Errou!!")
+elif maior:
+    print("Você Errou!! O seu chute foi maior do que o múmero secreto.")
+elif menor:
+    print("Você Errou!! O seu chute foi menor do que o múmero secreto.")
+
 
 print("Fim do Jogo!!")
