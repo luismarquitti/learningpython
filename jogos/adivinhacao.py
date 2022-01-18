f4bb5f2d0f05219ceb660f324957d5e5e157c19d
Luís Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2022-01-17 23:44:48 -0300

    Aula 6 do primeiro curso de Python 3 da Aula. Utilização de interpolação de strings e uso da função f-string

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,11 +4,13 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+for rodada in range(1, total_de_tentativas + 1):
+    print(f"Tentativa {rodada} de {total_de_tentativas}.")
+    chute = int(input("Digite um número entre 1 e 100: "))
+    if chute < 1 or chute > 100:
+        print("Você deve digiar um número entre 1 e 100!")
+        continue
     print("Você digitou ", chute)
 
     acertou = chute == numero_secreto
@@ -17,11 +19,11 @@ while (rodada <= total_de_tentativas):
 
     if acertou:
         print("Você Acertou!!")
+        break
     elif maior:
         print("Você Errou!! O seu chute foi maior do que o múmero secreto.")
     elif menor:
         print("Você Errou!! O seu chute foi menor do que o múmero secreto.")
 
-    rodada += 1
 
 print("Fim do Jogo!!")
