1a6db5420611681a983d517b142a36a9545308c3
Luís Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2022-01-18 22:23:18 -0300

    Aulas 4 e 5 do segundo curso de Python 3 da Aula. Implementando o fim do jogo, mensagem de erro e usando list comprehension para gerar a variável letras_acertadas.

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,26 +3,40 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_"for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while not enforcou and not acertou:
         print("Jogando...")
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou! Faltam {6-erros} tentativas.")
+
+        enforcou = erros == 6
+
+        acertou = "_" not in letras_acertadas
+
         print(letras_acertadas)
 
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 
