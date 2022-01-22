0484fbcb3f3d611b8f88d35b7f384b6fe39a09f0
Luís Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2022-01-21 23:07:13 -0300

    Aula 5 do curso de O.O. com Python 3 da Alura. Implementando getters ("@property") e setters ("@atributo.setter").

--- oo/conta.py
+++ oo/conta.py
@@ -16,6 +16,22 @@ class Conta:
     def saca(self, valor):
         self.__saldo -= valor
 
-    def transfere(self ,valor, destino):
+    def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
